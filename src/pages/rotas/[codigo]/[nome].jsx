2b06ae33ecf6218ddd0125b3852af36be329f3c5
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2023-07-22 16:34:49 -0300

    Finaizando exercicios

---
+++ src/pages/rotas/[codigo]/[nome].jsx
@@ -0,0 +1,20 @@
+import {useRouter} from 'next/router'
+import Link from 'next/link'
+
+export default function CodigoENome(){
+    const router = useRouter();
+    const codigo = router.query.codigo
+    const nome = router.query.nome
+
+    console.log(router);
+    return(
+        <div>
+            <h1>
+                Rotas / {codigo} / {nome}¢
+            </h1>
+            <Link href="/rotas" passHref>
+                <button>Voltar</button>
+            </Link>
+        </div>
+    )
+}
